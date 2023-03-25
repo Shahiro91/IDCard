dce526c4618efefb9b5397d06e85b0b5a03d1653
Maria Florencia Quintana <101899984+Shahiro91@users.noreply.github.com>
2023-03-24 22:05:58 -0300

    Add files via upload

---
+++ tareaReact/js.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    setTimeout(() => {
+        $('#variante').text('Diseñadora UX/UI.');
+    }, 4000);
+    setTimeout(() => {
+        $('#variante').text('Desarrolladora Front End.');
+    }, 8000);
+    setTimeout(() => {
+        $('#variante').text('Diseñadora UX/UI.');
+    }, 12000);
+});
